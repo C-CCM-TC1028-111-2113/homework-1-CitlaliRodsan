589a1d638187c3216dd5722c217f357bdf74d41f
CitlaliRodsan <88851035+CitlaliRodsan@users.noreply.github.com>
2021-08-21 22:39:30 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -3,6 +3,13 @@ def main():
     #Leer los datos
     pass
 
+    mensajes=int(input("Dame el número de mensajes: "))
+    megas=float(input("Dame el número de megas: "))
+    minutos=int(input("Dame el número de minutos: "))
+
+    mensual=(mensajes*0.8)+(megas*0.8)+(minutos*0.8)
+
+    print("El costo mensual es: ", mensual)
 
 if __name__ == '__main__':
     main()
