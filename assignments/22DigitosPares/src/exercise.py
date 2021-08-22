7896637b7aa87b9cabc61f80f4629b6d3e4ecdaf
CitlaliRodsan <88851035+CitlaliRodsan@users.noreply.github.com>
2021-08-21 23:39:32 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -2,6 +2,18 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+    num=input("Dame un número: ")
+    if int(num[0])%2==0:
+        if int(num[1])%2==0:
+            if int(num[2])%2==0:
+                if int(num[3])%2==0:
+                    print("El número de dígitos pares es: 4")
+            else:
+                print("El número de dígitos pares es: 3 ")
+        else:
+            print("El número de dígitos pares es: 2 ")
+    else:
+            print("El número de dígitos pares es: 1 ")
 
 if __name__ == '__main__':
     main()
