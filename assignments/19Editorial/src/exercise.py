54966b75ed52d9ccd92dd98b51ea8078a4240405
CitlaliRodsan <88851035+CitlaliRodsan@users.noreply.github.com>
2021-08-21 23:20:19 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -2,6 +2,13 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+    import math
+    palabras=int(input("Dame el número de palabras: "))
+
+
+    costo=math.ceil(palabras/475)*(60*0.9)
+
+    print("El costo de la publicaión es: ",costo)
 
 if __name__ == '__main__':
     main()
