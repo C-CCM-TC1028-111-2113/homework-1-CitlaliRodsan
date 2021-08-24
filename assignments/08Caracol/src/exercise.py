941f1c08622f30a12d023884326920c4b9c1e500
CitlaliRodsan <88851035+CitlaliRodsan@users.noreply.github.com>
2021-08-23 20:36:43 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,11 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-    min=float(input("Dame los minutos: "))
+  
+    min = float(input("Dame los minutos: "))
 
-    cm=(60*min*5.7)/10
+    cm = (60*min*5.7)/10
 
-    print("Centrímetos recorridos: ", cm)
+    print("Centrímetos recorridos:", cm)
     
 if __name__ == '__main__':
     main()
